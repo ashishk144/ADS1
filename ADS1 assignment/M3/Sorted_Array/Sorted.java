c839099bbaeea48bf20f38936498fc44edc14b7f
ashishk144 <ashishk1404@msitprogram.net>
2018-09-26 22:06:44 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS1 assignment/M3/Sorted_Array/Sorted.java
+++ ADS1 assignment/M3/Sorted_Array/Sorted.java
@@ -20,28 +20,49 @@ class Sorted {
     public String concat(int[] arr1, int[] arr2) {
         int len = arr1.length + arr2.length;
         int[] newarray = new int[len];
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
         while (i < len) {
-            if (j < arr1.length && j < arr2.length) {
-                if (arr1[j] < arr2[j]) {
+            // if (j < arr1.length && j < arr2.length) {
+            //     if (arr1[j] < arr2[j]) {
+            //         newarray[i] = arr1[j];
+            //         newarray[i + 1] = arr2[j];
+            //         i += 2;
+            //         j++;
+            //     } else {
+            //         newarray[i] = arr2[j];
+            //         newarray[i + 1] = arr1[j];
+            //         i += 2;
+            //         j++;
+            //     }
+            // } else if (j < arr1.length) {
+            //     newarray[i] = arr1[j];
+            //     i++;
+            //     j++;
+            // } else if (j < arr2.length) {
+            //     newarray[i] = arr2[j];
+            //     i++;
+            //     j++;
+            // }
+            if (j < arr1.length && k < arr2.length) {
+                if (arr1[j] <= arr2[k]) {
                     newarray[i] = arr1[j];
-                    newarray[i + 1] = arr2[j];
-                    i += 2;
                     j++;
+                    i++;
                 } else {
-                    newarray[i] = arr2[j];
-                    newarray[i + 1] = arr1[j];
-                    i += 2;
-                    j++;
+                    newarray[i] = arr2[k];
+                    k++;
+                    i++;
                 }
             } else if (j < arr1.length) {
                 newarray[i] = arr1[j];
                 i++;
                 j++;
-            } else if (j < arr2.length) {
-                newarray[i] = arr2[j];
+            } else if (k < arr2.length) {
+                newarray[i] = arr2[k];
                 i++;
-                j++;
+                k++;
             }
         }
         return Arrays.toString(newarray).replaceAll("[\\]\\[ ]", "");
