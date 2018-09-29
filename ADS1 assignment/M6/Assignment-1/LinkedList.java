ec9af29a8d4abda052573a76d420c2641548be2d
ashishk144 <ashishk1404@msitprogram.net>
2018-09-29 12:24:53 +0530

    "check_style_score -> 12 of 12 passed. style score: 1/1 "

--- ADS1 assignment/M6/Assignment-1/LinkedList.java
+++ ADS1 assignment/M6/Assignment-1/LinkedList.java
@@ -1,13 +1,30 @@
 public class LinkedList {
+    /**
+     * Class for node.
+     */
 	private class Node {
 		int data;
 		Node next;
+        /**
+         * Constructs the object.
+         */
 		Node() {
 
 		}
+        /**
+         * Constructs the object.
+         *
+         * @param      data  The data
+         */
 		Node(int data) {
 			this.data = data;
 		}
+        /**
+         * Constructs the object.
+         *
+         * @param      data  The data
+         * @param      n     { parameter_description }
+         */
 		Node(int data, Node n) {
 			this.data = data;
 			this.next = n;
@@ -15,9 +32,17 @@ public class LinkedList {
 	}
 	private Node head, tail;
 	private int size;
+    /**
+     * Constructs the object.
+     */
 	LinkedList() {
 
 	}
+    /**
+     * Pushes an object at tail.
+     *
+     * @param      data  The data
+     */
 	public void push(int data) {
 		if (head == null) {
 			head = new Node(data);
@@ -30,6 +55,11 @@ public class LinkedList {
 		tail = node;
 		size++;
 	}
+    /**
+     * Pushes at head.
+     *
+     * @param      data  The data
+     */
     public void pushHead(int data) {
         if (head == null) {
             head = new Node(data);
@@ -41,6 +71,11 @@ public class LinkedList {
         head = node;
         size++;
     }
+    /**
+     * Pops out the object.
+     *
+     * @return     { description_of_the_return_value }
+     */
 	public int pop() {
 		if (head != null) {
 			Node temp = head;
@@ -57,9 +92,19 @@ public class LinkedList {
 		}
 		return 0;
 	}
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
 	public boolean isEmpty() {
 		return size == 0;
 	}
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
 	public String toString() {
 		Node thead = head;
 		String s = "";
@@ -69,6 +114,11 @@ public class LinkedList {
 		}
 		return s;
 	}
+    /**
+     * Gets the size.
+     *
+     * @return     The size.
+     */
 	public int getSize() {
 		return size;
 	}
