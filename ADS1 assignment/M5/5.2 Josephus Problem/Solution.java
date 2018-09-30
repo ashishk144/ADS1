3ec018571427919a5c751a28b8159c395336f568
ashishk144 <ashishk1404@msitprogram.net>
2018-09-30 19:39:38 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS1 assignment/M5/5.2 Josephus Problem/Solution.java
+++ ADS1 assignment/M5/5.2 Josephus Problem/Solution.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
  * Class to handle inputs and deliver outputs.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * The main function to take inputs and deliver outputs.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Problem l;
+    }
+    /**
+     * The main function to take inputs and deliver outputs.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Problem l;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             String[] tokens = sc.nextLine().split(" ");
-            l = new Problem(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+            l = new Problem(Integer.parseInt(tokens[0]),
+                Integer.parseInt(tokens[1]));
             l.josephproblem();
         }
-	}
+    }
 }
