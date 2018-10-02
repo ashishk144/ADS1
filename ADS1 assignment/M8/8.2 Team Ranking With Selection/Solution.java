3b98f108a107e7f0bc9c8e80f5c579cc9bfa6c08
ashishk144 <ashishk1404@msitprogram.net>
2018-10-02 18:36:09 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS1 assignment/M8/8.2 Team Ranking With Selection/Solution.java
+++ ADS1 assignment/M8/8.2 Team Ranking With Selection/Solution.java
@@ -2,28 +2,36 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+public final class Solution {
+    /**
+     * Magic.
+     */
+    private static final int TEN = 10;
+    /**
+     * Magic
+     */
+    private static final int THREE = 3;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * Main function to handle test cases and deliver output.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Team[] teams = new Team[10];
-		int size = 0;
-		Scanner scan = new Scanner(System.in);
-		Selection s = new Selection();
-		while (scan.hasNext()) {
-			String[] line = scan.nextLine().split(",");
-			teams[size++] = new Team(line[0], Integer.parseInt(line[1]),
-			      Integer.parseInt(line[2]), Integer.parseInt(line[3]));
-		}
-		System.out.println(s.winner(teams, size));
-	}
+    }
+    /**
+     * Main function to handle test cases and deliver output.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Team[] teams = new Team[TEN];
+        int size = 0;
+        Scanner scan = new Scanner(System.in);
+        Table t = new Table();
+        while (scan.hasNext()) {
+            String[] line = scan.nextLine().split(",");
+            teams[size++] = new Team(line[0], Integer.parseInt(line[1]),
+                     Integer.parseInt(line[2]), Integer.parseInt(line[THREE]));
+        }
+        System.out.println(t.leaderboard(teams, size));
+    }
 }
