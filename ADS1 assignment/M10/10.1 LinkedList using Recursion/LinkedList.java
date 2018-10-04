308f50f64b66fc09bd8214c3d216f611b01ee7b0
ashishk144 <ashishk1404@msitprogram.net>
2018-10-04 22:35:58 +0530

    "ads1_m10_a1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1 assignment/M10/10.1 LinkedList using Recursion/LinkedList.java
@@ -0,0 +1,63 @@
+public class LinkedList<E> {
+    private class Node {
+        E data;
+        Node next;
+        Node() {
+
+        }
+        Node(E data) {
+            this.data = data;
+        }
+        Node(E data, Node n) {
+            this.data = data;
+            this.next = n;
+        }
+    }
+    Node head;
+    int size;
+    public LinkedList()  {
+        head = null;
+        size = 0;
+    }
+    public void insertAt(E data, int point) throws Exception {
+        if (point < 0 || point > size) {
+            throw new Exception("Can't insert at this position");
+        }
+        Node n = new Node(data);
+        head = insertAt(n, head, point, 0);
+    }
+    public Node insertAt(Node node, Node first, int pos, int count) {
+        if (pos == count) {
+            node.next = first;
+            size += 1;
+            System.out.println(size);
+            return node;
+        } else {
+            first.next = insertAt(node, first.next, pos, count + 1);
+            return first;
+        }
+    }
+    public void reverse() throws Exception {
+        if(head == null) {
+            throw new Exception("No elements to reverse.");
+        }
+        reverse(head, null);
+    }
+    public void reverse(Node current, Node previous) {
+        if(current != null) {
+            reverse(current, current.next);
+            current.next = previous;
+        } else {
+            head = previous;
+        }
+    }
+    public String toString() {
+        Node thead = head;
+        String s = "";
+        while (thead != null) {
+            s += thead.data + ", ";
+            thead = thead.next;
+        }
+        return s.substring(0, s.length() - 2);
+    }
+}
---
+++ ADS1 assignment/M10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Class for Solution
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * Main function to handle inputs and deliver outputs.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        LinkedList<String> l = new LinkedList<String>();
+        while (scan.hasNext()) {
+            String[] inp = scan.nextLine().split(" ");
+            switch (inp[0]) {
+            case "insertAt":
+                try {
+                    l.insertAt(inp[2], Integer.parseInt(inp[1]));
+                    System.out.println(l);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case "reverse":
+                try {
+                    l.reverse();
+                    System.out.println(l);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
