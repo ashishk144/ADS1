6e3a37305cc176d383a182d0f9f8ea68edfe70c3
ashishk144 <ashishk1404@msitprogram.net>
2018-10-08 23:39:20 +0530

    "ads1_m13_a1: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1 assignment/M13/13.1 isMinHeap/Solution.java
+++ ADS1 assignment/M13/13.1 isMinHeap/Solution.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
+
 /**
  * Class for solution.
  */
@@ -24,20 +27,23 @@ public class Solution {
             String[] line = inp.split(",");
             if (inp.length() < 1) {
                 System.out.println("false");
-                break;
-            }
-            if (type.equals("String")) {
-                MinPQ<String> m = new MinPQ();
-                System.out.println(m.isHeap(line));
-            } else if (type.equals("Float")) {
-                MinPQ<Float> m = new MinPQ();
-                System.out.println(m.isHeap(line));
-            } else if (type.equals("Double")) {
-                MinPQ<Double> m = new MinPQ();
-                System.out.println(m.isHeap(line));
-            } else if (type.equals("Integer")) {
-                MinPQ<Integer> m = new MinPQ();
-                System.out.println(m.isHeap(line));
+            } else {
+                if (type.equals("String")) {
+                    MinPQ<String> m = new MinPQ();
+                    System.out.println(m.isHeap(line));
+                } else if (type.equals("Float")) {
+                    MinPQ<Float> m = new MinPQ();
+                    Float[] integerArray = Arrays.copyOf(line, line.length, Float[].class);
+                    System.out.println(m.isHeap(line));
+                } else if (type.equals("Double")) {
+                    MinPQ<Double> m = new MinPQ();
+                    Double[] integerArray = Arrays.copyOf(line, line.length, Double[].class);
+                    System.out.println(m.isHeap(line));
+                } else if (type.equals("Integer")) {
+                    MinPQ<Integer> m = new MinPQ();
+                    Integer[] integerArray = Arrays.copyOf(line, line.length, Integer[].class);
+                    System.out.println(m.isHeap(line));
+                }
             }
             i++;
         }
