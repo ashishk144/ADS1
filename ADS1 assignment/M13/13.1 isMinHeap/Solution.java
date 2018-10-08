91f480b2d3b22044071a413765187b454c470e63
ashishk144 <ashishk1404@msitprogram.net>
2018-10-08 23:50:31 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1 assignment/M13/13.1 isMinHeap/Solution.java
+++ ADS1 assignment/M13/13.1 isMinHeap/Solution.java
@@ -22,27 +22,40 @@ public class Solution {
         String type = scan.nextLine();
         int n = Integer.parseInt(scan.nextLine());
         int i = 0;
+        int size;
         while (i < n) {
             String inp = scan.nextLine();
-            String[] line = inp.split(",");
+            String[] input = inp.split(",");
             if (inp.length() < 1) {
                 System.out.println("false");
             } else {
                 if (type.equals("String")) {
                     MinPQ<String> m = new MinPQ();
-                    System.out.println(m.isHeap(line));
+                    System.out.println(m.isHeap(input));
                 } else if (type.equals("Float")) {
                     MinPQ<Float> m = new MinPQ();
-                    Float[] integerArray = Arrays.copyOf(line, line.length, Float[].class);
-                    System.out.println(m.isHeap(line));
+                    size = 0;
+                    Float[] floatArray = new Float[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        floatArray[size++] = Float.parseFloat(input[j]);
+                    }
+                    System.out.println(m.isHeap(floatArray));
                 } else if (type.equals("Double")) {
                     MinPQ<Double> m = new MinPQ();
-                    Double[] integerArray = Arrays.copyOf(line, line.length, Double[].class);
-                    System.out.println(m.isHeap(line));
+                    size = 0;
+                    Double[] doubleArray = new Double[input.length];
+                    for (int j = 0; j < input.length; j++) {
+                        doubleArray[size++] = Double.parseDouble(input[j]);
+                    }
+                    System.out.println(m.isHeap(doubleArray));
                 } else if (type.equals("Integer")) {
                     MinPQ<Integer> m = new MinPQ();
-                    Integer[] integerArray = Arrays.copyOf(line, line.length, Integer[].class);
-                    System.out.println(m.isHeap(line));
+                    Integer[] integerArray = new Integer[input.length];
+                    size = 0;
+                    for (int j = 0; j < input.length; j++) {
+                        integerArray[size++] = Integer.parseInt(input[j]);
+                    }
+                    System.out.println(m.isHeap(integerArray));
                 }
             }
             i++;
