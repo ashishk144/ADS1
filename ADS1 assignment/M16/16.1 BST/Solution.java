69e4825da05d9876368b67e075d3d2e32e7c7316
ashishk144 <ashishk1404@msitprogram.net>
2018-10-12 17:09:44 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1 assignment/M16/16.1 BST/Solution.java
+++ ADS1 assignment/M16/16.1 BST/Solution.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
  */
 public final class Solution {
     /**
+     * Magic Number.
+     */
+    private final static int THREE = 3;
+    /**
+     * Magic Number.
+     */
+    private final static int FOUR = 4;
+    /**
      * Constructs the object.
      */
     private Solution() {
@@ -20,14 +28,14 @@ public final class Solution {
         Bst<Book, Integer> b = new Bst();
         while (scan.hasNext()) {
             String[] inp = scan.nextLine().split(",");
-            switch(inp[0]) {
+            switch (inp[0]) {
                 case "put":
                     b.put(new Book(inp[1], inp[2], Double.parseDouble(
-                        inp[3])), Integer.parseInt(inp[4]));
+                        inp[THREE])), Integer.parseInt(inp[FOUR]));
                     break;
                 case "get":
-                    System.out.println(b.get(new Book(inp[1], inp[2], Double.parseDouble(
-                        inp[3]))));
+                    System.out.println(b.get(new Book(inp[1], inp[2],
+                        Double.parseDouble(inp[THREE]))));
                     break;
                 default:
                     break;
