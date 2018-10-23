0193cf703285d3044fb28b8b5991ac75c143366d
ashishk144 <ashishk1404@msitprogram.net>
2018-10-23 20:39:35 +0530

    ": 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS1 assignment/M20/20.1 BST/Book.java
@@ -0,0 +1,50 @@
+// import java.util.;
+/**
+ * Class for book.
+ */
+public class Book implements Comparable<Book> {
+    /**
+     * Book name.
+     */
+    private String name;
+    /**
+     * Author name.
+     */
+    private String author;
+    /**
+     * Price.
+     */
+    private Double price;
+    /**
+     * Constructs the object.
+     */
+    public Book() {
+
+    }
+    /**
+     * Constructor for book.
+     */
+    public Book(final String book, final String autname, final Double cost) {
+        this.name = book;
+        this.author = autname;
+        this.price = cost;
+    }
+    /**
+     * Compares 2 parameters.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Book b) {
+        return this.name.compareTo(b.name);
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String s = "";
+        s += this.name + ", " + this.author + ", " + this.price;
+        return s;
+    }
+}
