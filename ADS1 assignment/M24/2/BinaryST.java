d26271be040dd4af73c246ad3bc655f99637bc7f
ashishk144 <ashishk1404@msitprogram.net>
2018-10-27 12:44:37 +0530

    "m24_a2_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS1 assignment/M24/2/BinaryST.java
+++ ADS1 assignment/M24/2/BinaryST.java
@@ -130,7 +130,7 @@ public class BinaryST<Key extends Comparable<Key>, Value> extends Queue {
         } else if (cmp > 0) {
             x.right = put(x.right, key, val);
         } else {
-            x.val   = val;
+            return new Node(key, val, 1);
         }
         x.size = 1 + size(x.left) + size(x.right);
         return x;
